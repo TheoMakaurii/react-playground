4b1d5ec5241628884c976bac2005e7876ad83021
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-26 21:28:23 -0400

    tab practice

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
 import React from 'react';
-import Split from './composition/Split';
+// import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip';
+// import Tooltip from './composition/Tooltip';
 import TheDate from './composition/state/TheDate';
 import Counter from './composition/state/Counter';
 import HelloWorld from './composition/state-drills/helloWorld';
 import Bomb from './composition/state-drills/bomb';
-import RussianRoulette from './composition/state-drills/russianRoulette'
+import RussianRoulette from './composition/state-drills/russianRoulette';
+import Tabs from './composition/state/Tabs'
+
 
 // make 2 tooltips here and another inside the App directly
-const firstTooltip = (
-  <Tooltip message='tooltip message'>
-   ipsum
-  </Tooltip>
-)
-const secondTooltip = (
-  <Tooltip message='another tooltip message'>
-    officiis
-  </Tooltip>
-)
+// const firstTooltip = (
+//   <Tooltip message='tooltip message'>
+//    ipsum
+//   </Tooltip>
+// )
+// const secondTooltip = (
+//   <Tooltip message='another tooltip message'>
+//     officiis
+//   </Tooltip>
+// )
+const tabsProp = [
+  { name: 'first tab',
+    content: 'twinkle, twinkle' },
+  { name: 'second tab',
+    content: 'little star' },
+  { name: 'third tab',
+    content: 'how I wonder' },
+  { name: 'fourth tab',
+    content: 'what you are' },
+  { name: 'fifth tab',
+    content: 'up above the world so high' },
+  { name: 'sixth tab',
+    content: 'like a diamond in the sky' },
+  { name: 'seventh tab',
+    content: 'twinkle, twinkle' },
+  { name: 'eigth tab',
+    content: 'little star' },
+  { name: 'ninth tab',
+    content: 'how I wonder' },
+  { name: 'tenth tab',
+    content: 'what you are...' },
+];
 
 function App() {
   return (
     <main className='App'>
-      <Split className='left' flexBasis='2'>
-        This is the content for the left Split. <br />
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right Split.
-      </Split>
+
       <div className= "State-Prac">
       <TheDate/>
       </div>
@@ -48,8 +63,13 @@ function App() {
       <div className = "Gun">
         <RussianRoulette bulletInChamber={6} />
       </div>
+      <div className="Tabs">
+      <Tabs tabs={tabsProp} />
+      </div>
+        
     </main>
   )
 }
 
 export default App
+
